1b460933446d8862477a647d11bb3b3cba3bedb4
Nisarg <nisargvv@gmail.com>
2021-01-04 20:12:29 +0000

    Finish queries 2

---
+++ 2_queries_joins/2_cohorts_assignment_duration.sql
@@ -0,0 +1,7 @@
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+INNER JOIN students
+ON student_id = students.id
+INNER JOIN cohorts
+ON cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
